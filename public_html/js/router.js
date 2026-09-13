91b42f05b0b0981f55cb5329c33f804001f87748
Matheus <Matheus@CASA-B7H4X33>
2026-09-13 18:17:57 +0100

    13.09 Adicionado js

---
+++ public_html/js/router.js
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+async function loadPage(page){
+    const response = await fetch(page);
+    const html = await response.text();
+    document.getElementById("main-content").innerHTML = html;
+}
