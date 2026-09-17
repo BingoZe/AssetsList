1727c41b7c26d8ca3eafa2cc161d7783ba8a8c72
Matheus.Aquiles <Matheus.Aquiles@CH-FSX8NC4.CROWEHORWATH.COM>
2026-09-17 17:58:42 +0100

    Mais adicoes

--- public_html/js/router.js
+++ public_html/js/router.js
@@ -10,3 +10,14 @@ async function loadPage(page){
     };
     
     
+function openTab(evt,tabName){
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    
+    for(i = 0; i<tabcontent.length;i++){
+        tabcontent[i].style.display="none";
+    }
+        
+}
+    
