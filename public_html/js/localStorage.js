597ed232d3971b0a4a1495c771a1c4f44a5de96e
Matheus <Matheus@CASA-B7H4X33>
2026-09-23 23:36:35 +0100

    Modificado add asset

--- public_html/js/localStorage.js
+++ public_html/js/localStorage.js
@@ -1,38 +1,4 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-
-/*pageloader on index*/
-async function loadPage(page){
-    const response = await fetch(page);
-    const html = await response.text();
-     
-    document.getElementById("main-content").innerHTML = html;
-     
-    const generalTab = document.getElementById("generalTab");
-     
-    if(generalTab){
-    generalTab.style.display = "block";
-}
-}
-    
-/* open tab settings*/
-
-function openTab(evt, tabName) {
- 
-    let tabcontent = document.getElementsByClassName("tabcontent");
-     
-    for(let i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-    }
-     
-    document.getElementById(tabName).style.display = "block";
-     
-    evt.preventDefault();
-}
-    
+   
 /*localstorage*/
 
 const assetForm = document.querySelector(".asset-form");
--- public_html/js/openTab.js
+++ public_html/js/openTab.js
@@ -1,23 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-
-/*pageloader on index*/
-async function loadPage(page){
-    const response = await fetch(page);
-    const html = await response.text();
-     
-    document.getElementById("main-content").innerHTML = html;
-     
-    const generalTab = document.getElementById("generalTab");
-     
-    if(generalTab){
-    generalTab.style.display = "block";
-}
-}
-    
 /* open tab settings*/
 
 function openTab(evt, tabName) {
